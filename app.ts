2dbacbb6fce3476856e5d277d28cbdf2572967e2
geixco <geixco@protonmail.com>
2022-03-14 10:29:48 -0400

    create a proper interface for the json data

--- app.ts
+++ app.ts
@@ -1,7 +1,19 @@
-// import { exec } from 'child_process';
+import { exec } from 'child_process';
 import { app, BrowserWindow } from 'electron';
-// @ts-ignore next-line
-import * as data from './data.json';
+
+import * as json from './data.json';
+
+interface AppData {
+  name: string;
+  description: string;
+  url: string;
+  platform: "windows" | "linux" | "mac";
+  install: "true" | "false";
+  start: "true" | "false";
+  desktop: "true" | "false";
+}
+
+const data = json as AppData;
 
 function init() {
   let url: string;
@@ -15,7 +27,7 @@ function init() {
     title: data?.name || "Electrify",
   }).loadURL(url)
 }
-// dick
+
 app.whenReady().then(() => {
   init();
 
@@ -28,4 +40,53 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 })
 
-// exec('npm run package');
+exec('npm run package');
+
+if (data.install) {
+  // if linux, move packaged file to opt/electrify/appname
+  if (data.platform === "linux") {
+    exec(`mkdir -p /opt/electrify/${data.name}`);
+    exec(`mv out/electrify-${data.platform}-x64/${data.name} /opt/electrify/${data.name}`);
+  }
+  // if windows, move packaged file to C:\Users\username\AppData\Roaming\electrify\appname
+  else if (data.platform === "windows") {
+    exec(`mkdir -p %APPDATA%\\electrify\\${data.name}`);
+    exec(`mv out/electrify-${data.platform}-x64\\${data.name} %APPDATA%\\electrify\\${data.name}`);
+  }
+} else if (data.start) {
+  // if linux, create a .desktop file in /usr/share/applications
+  if (data.platform === "linux") {
+    exec(`echo "[Desktop Entry]
+Name=${data.name}
+Comment=${data.description}
+Exec=out/electrify-${data.platform}-x64/${data.name}
+Icon=out/electrify-${data.platform}-x64/${data.name}/icon.png
+Terminal=false
+Type=Application" > /usr/share/applications/${data.name}.desktop`);
+  }
+  // if windows, create a .lnk file in the start menu
+  else if (data.platform === "windows") {
+    // TODO: create .lnk file in start menu
+  }
+} else if (data.desktop) {
+  // if linux, create a .desktop file in /home/username/Desktop
+  if (data.platform === "linux") {
+    exec(`echo "[Desktop Entry]
+Name=${data.name}
+Comment=${data.description}
+Exec=out/electrify-${data.platform}-x64/${data.name}
+Icon=out/electrify-${data.platform}-x64/${data.name}/icon.png
+Terminal=false
+Type=Application" > /home/username/Desktop/${data.name}.desktop`);
+  }
+  else if (data.platform === "windows") {
+    // Create .lnk file and store it in C:\Users\username\Desktop
+    exec(`echo "[Desktop Entry]
+Name=${data.name}
+Comment=${data.description}
+Exec=out/electrify-${data.platform}-x64\\${data.name}
+Icon=out/electrify-${data.platform}-x64\\${data.name}\\icon.png
+Terminal=false
+Type=Application" > C:\\Users\\username\\Desktop\\${data.name}.desktop`);
+  }
+}
