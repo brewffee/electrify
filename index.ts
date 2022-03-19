98f23e170ec71dd6105b71189ea07a96fb74b76b
geixco <geixco@protonmail.com>
2022-03-19 16:00:03 -0400

    arg support (i forgor)

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { spawn } from 'child_process';
 
-const child = spawn('./main', [], {
+const child = spawn('./main', process.argv.splice(2), {
     stdio: ['inherit', 'inherit', 'inherit', 'ipc']
 });
